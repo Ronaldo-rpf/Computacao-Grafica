54d7eb74609f7b888984f599e2f2a078319473ee
Ronaldo Porto <portofilhoronaldo@gmail.com>
2025-09-23 18:18:20 -0300

    ChromaKey

--- ChromaKey/chromaKey.c
+++ ChromaKey/chromaKey.c
@@ -1,12 +1,13 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-typedef struct{
+/// @brief Estrutura contendo os valores RGB de um pixel.
+typedef struct {
     int R;
     int G;
     int B;
-}RGB;
+} RGB;
 
 FILE *arqBackground, *arqForeground, *arqFinal;
 int Rk, Gk, Bk, limSup, limInf;
@@ -18,14 +19,25 @@ char tipo[4];
 
 RGB **imgBack, **imgFore;
 
-void abrirArquivos(int argc, char** argv);
+/// @brief Abre as imagens e recebe os valores da chave e dos limites.
+void abrirArquivos(int argc, char **argv);
+
+/// @brief Verifica as informações dos cabeçalhos das imagens.
 void lerCabecalhoImagens();
+
+/// @brief Armazena os pixels das imagens em matrizes.
 void lerImagens();
+
+/// @brief Retorna a distância de um pixel até os valores de chave.
 int distanciaEuclidiana(int R, int G, int B);
+
+/// @brief Processa a imagem final aplicando o chroma key.
 void chromaKey();
+
+/// @brief Fecha os arquivos.
 void fecharArquivos();
 
-int main (int argc, char** argv){
+int main(int argc, char **argv) {
     abrirArquivos(argc, argv);
     lerCabecalhoImagens();
     lerImagens();
@@ -34,20 +46,21 @@ int main (int argc, char** argv){
     return 0;
 }
 
-void abrirArquivos(int argc, char** argv){
-    if (argc <= 8){
-        printf("Forma correta de entrada: <Nome do programa> <Imagem de background> <Imagem de foreground> <Imagem de saida> <Rk> <Gk> <Bk> <Limite superior> <Limite inferior>\n");
+void abrirArquivos(int argc, char **argv) {
+    if (argc <= 8) {
+        printf("Forma correta de entrada: <Nome do programa> <Imagem de background> <Imagem de foreground> <Imagem de "
+               "saida> <Rk> <Gk> <Bk> <Limite superior> <Limite inferior>\n");
         exit(1);
     }
-    if ((arqBackground = fopen(argv[1], "r")) == NULL){
+    if ((arqBackground = fopen(argv[1], "r")) == NULL) {
         printf("Erro ao abrir imagem de background.\n");
         exit(1);
     }
-    if ((arqForeground = fopen(argv[2], "r")) == NULL){
+    if ((arqForeground = fopen(argv[2], "r")) == NULL) {
         printf("Erro ao abrir imagem de foreground.\n");
         exit(1);
     }
-    if ((arqFinal = fopen(argv[3], "w")) == NULL){
+    if ((arqFinal = fopen(argv[3], "w")) == NULL) {
         printf("Erro ao abrir imagem de saida.\n");
         exit(1);
     }
@@ -61,7 +74,7 @@ void abrirArquivos(int argc, char** argv){
     return;
 }
 
-void lerCabecalhoImagens(){
+void lerCabecalhoImagens() {
     int temp;
     fscanf(arqForeground, "%s", tipo);
     fscanf(arqForeground, "%d %d", &nColFore, &nLinFore);
@@ -70,50 +83,50 @@ void lerCabecalhoImagens(){
     fscanf(arqBackground, "%s", tipo);
     fscanf(arqBackground, "%d %d", &nColBack, &nLinBack);
     fscanf(arqBackground, "%d", &maiorRGB);
-    
-    if(nColFore > nColBack){
+
+    if (nColFore > nColBack) {
         printf("Erro: Imagem de foreground maior que imagem de background.\n");
         exit(1);
     }
-    if(nLinFore > nLinBack){
+    if (nLinFore > nLinBack) {
         printf("Erro: Imagem de foreground maior que imagem de background.\n");
         exit(1);
     }
     return;
 }
 
-void lerImagens(){
-    imgBack = (RGB**) calloc(nLinBack, sizeof(RGB*));
-    imgFore = (RGB**) calloc(nLinFore, sizeof(RGB*));
-    if (imgBack == NULL || imgFore == NULL){
+void lerImagens() {
+    imgBack = (RGB **)calloc(nLinBack, sizeof(RGB *));
+    imgFore = (RGB **)calloc(nLinFore, sizeof(RGB *));
+    if (imgBack == NULL || imgFore == NULL) {
         printf("Erro ao alocar imagem final.\n");
         exit(1);
     }
-    for(int i = 0; i < nLinBack; i++){
-        imgBack[i] = (RGB*) calloc(nColBack, sizeof(RGB));
-        if(imgBack[i] == NULL){
+    for (int i = 0; i < nLinBack; i++) {
+        imgBack[i] = (RGB *)calloc(nColBack, sizeof(RGB));
+        if (imgBack[i] == NULL) {
             printf("Erro ao alocar imagem final.\n");
             exit(1);
         }
     }
-    for(int i = 0; i < nLinFore; i++){
-        imgFore[i] = (RGB*) calloc(nColFore, sizeof(RGB));
-        if(imgFore[i] == NULL){
+    for (int i = 0; i < nLinFore; i++) {
+        imgFore[i] = (RGB *)calloc(nColFore, sizeof(RGB));
+        if (imgFore[i] == NULL) {
             printf("Erro ao alocar imagem final.\n");
             exit(1);
         }
     }
 
-    for(int i = 0; i < nLinBack; i++){
-        for(int j = 0; j < nColBack; j++){
+    for (int i = 0; i < nLinBack; i++) {
+        for (int j = 0; j < nColBack; j++) {
             fscanf(arqBackground, "%d", &imgBack[i][j].R);
             fscanf(arqBackground, "%d", &imgBack[i][j].G);
             fscanf(arqBackground, "%d", &imgBack[i][j].B);
         }
     }
 
-    for(int i = 0; i < nLinFore; i++){
-        for(int j = 0; j < nColFore; j++){
+    for (int i = 0; i < nLinFore; i++) {
+        for (int j = 0; j < nColFore; j++) {
             fscanf(arqForeground, "%d", &imgFore[i][j].R);
             fscanf(arqForeground, "%d", &imgFore[i][j].G);
             fscanf(arqForeground, "%d", &imgFore[i][j].B);
@@ -122,30 +135,28 @@ void lerImagens(){
     return;
 }
 
-int distanciaEuclidiana(int R, int G, int B){
+int distanciaEuclidiana(int R, int G, int B) {
     return (pow((R - Rk), 2) + pow((G - Gk), 2) + pow((B - Bk), 2));
 }
 
-void chromaKey(){
+void chromaKey() {
     fprintf(arqFinal, "%s\n", tipo);
     fprintf(arqFinal, "%d %d\n", nColFore, nLinFore);
     fprintf(arqFinal, "%d\n", maiorRGB);
     int d;
     RGB media;
-    for(int i = 0; i < nLinFore; i++){
-        for(int j = 0; j < nColFore; j++){
+    for (int i = 0; i < nLinFore; i++) {
+        for (int j = 0; j < nColFore; j++) {
             d = distanciaEuclidiana(imgFore[i][j].R, imgFore[i][j].G, imgFore[i][j].B);
-            if (d < limInf){
+            if (d < limInf) {
                 fprintf(arqFinal, "%d\n", imgBack[i][j].R);
                 fprintf(arqFinal, "%d\n", imgBack[i][j].G);
                 fprintf(arqFinal, "%d\n", imgBack[i][j].B);
-            }
-            else if (d > limSup){
+            } else if (d > limSup) {
                 fprintf(arqFinal, "%d\n", imgFore[i][j].R);
                 fprintf(arqFinal, "%d\n", imgFore[i][j].G);
                 fprintf(arqFinal, "%d\n", imgFore[i][j].B);
-            }
-            else{
+            } else {
                 media.R = (imgFore[i][j].R + imgBack[i][j].R) / 2;
                 media.G = (imgFore[i][j].G + imgBack[i][j].G) / 2;
                 media.B = (imgFore[i][j].B + imgBack[i][j].B) / 2;
@@ -158,7 +169,7 @@ void chromaKey(){
     return;
 }
 
-void fecharArquivos(){
+void fecharArquivos() {
     fclose(arqBackground);
     fclose(arqForeground);
     fclose(arqFinal);
